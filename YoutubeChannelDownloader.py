1e2eaca6cefc47a1d1ba8dbdb8fd4a257e5719ec
maitris <maitrishastri2016@gmail.com>
2021-06-11 22:52:45 +0530

    Adding file YoutubeChannelDownloader
    Added a method to download audio and video and merge them using ffmpeg

---
+++ YoutubeChannelDownloader.py
@@ -0,0 +1,28 @@
+from pytube import Youtube
+from pytube import Channel
+from slugify import slugify
+import csv
+import ffmpeg
+
+
+def DownloadVideo(video_link, folder_path, resolution=None):
+	if(resolution == None):
+		video_file = Youtube(video_link).streams.order_by('resolution').desc().first().download()
+	else:
+		video_file = Youtube(video_link).streams.filter(res=resolution).order_by('resolution').desc().first().download()
+
+	video_name = slugify(video_file.replace(".webm","").split("/")[-1])
+
+	print("Video downloaded")
+
+	audio_file = Youtube(video_link).streams.filter(only_audio=True).order_by('abr').desc().first().download(filename_prefix="audio_")
+
+	print("Audio downloaded")
+
+	source_audio = ffmpeg.input(audio_file)
+	source_video = ffmpeg.input(video_file)
+	ffmpeg.concat(source_video, source_audio, v=1, a=1).output(f"{folder_path}/{video_name}.mp4").run()
+
+	print("Audio Video merging done")
+	return None
+
